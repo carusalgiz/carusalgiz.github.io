944c3ad0bf8ba10610a567f3f633fc880da49a41
Karina Milovanova <frankarenyan@gmail.com>
2018-06-12 13:59:51 +1000

    fisrt commit

---
+++ script.js
@@ -0,0 +1,52 @@
+function selected(el) {
+    var items = document.getElementsByClassName("sets");
+    for(var i = 0; i < items.length;i++){
+        items.item(i).style.removeProperty('background-color');
+        items.item(i).style.removeProperty('border-left');
+    }
+	el.style.setProperty('background-color', "#e6e7e8");
+    el.style.setProperty('border-left', "6px #3e8ecf solid");
+
+    switch(el.id){
+        case 'sources':
+            document.getElementById('center-part').innerHTML='<a>Период активности(мин):  </a>' +
+                '<input type="text"><input type="submit" value="Назначить" class="buttonStyle"><input type="button" value="Добавить" class="buttonStyle" onclick="newSource()" style="float: right">';
+            break;
+        case 'ip':
+            //text
+            break;
+        case 'parcer':
+            //text
+            break;
+        case 'activeTasks':
+            //text
+            break;
+        case 'loadOnNetwork':
+            //text
+            break;
+        case 'stats':
+            //text
+            break;
+        case 'errors':
+            //text
+            break;
+    }
+}
+
+function newSource() {
+    document.getElementById('center-part').innerHTML = '<a>Создание нового источника:</a><input type="button" class="buttonStyle" onclick="newCity()" value="Добавить город" style="margin-left: 108px; width: 140px; float: right"><br>';
+    newCity();
+}
+function newNameSource() {
+    document.getElementById('inputFields').innerHTML+='<input type="text" style="margin-left: 95px"><br>';
+}
+function newCity() {
+    document.getElementById('center-part').innerHTML+='<br><div style="float: left; line-height: 1.4em; margin-left: 20px"><a>Название:  </a><br><br>' +
+        '<a>Город:  </a><br>' +
+        '<a>site_url:  </a><br>' +
+        '<a>host:  </a><br>' +
+        '<a>page_count:  </a><br>' +
+        '<a>pause:  </a><br>' +
+        '<a>source_list:  </a><br></div>' +
+        '<div id="inputFields"><input type="text"><br><br><input type="text"><br><input type="text"><br><input type="text"><br><input type="text"><br><input type="text"><br><input type="text"><input type="button" value="+" class="roundButton" onclick="newNameSource()"><br></div></div>';
+}
