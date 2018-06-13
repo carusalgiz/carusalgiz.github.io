e779aa3a7ec0eb87e3957c0cc4e6779f74db8df9
Karina Milovanova <frankarenyan@gmail.com>
2018-06-13 13:28:41 +1000

    3

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function selected(el) {
 
     switch(el.id){
         case 'sources':
-            document.getElementById('center-part').innerHTML='<a>Период активности(мин):  </a>' +
-                '<input type="text" class="inputField"><input type="submit" value="Назначить" class="buttonStyle"><input type="button" value="Добавить источник" class="buttonStyle" onclick="newSource()">';
+            document.getElementById('center-top').innerHTML='<a>Период активности(мин):  </a>' +
+                '<input type="text"><input type="submit" value="Назначить" class="buttonStyle"><input type="button" value="Добавить источник" class="buttonStyle" onclick="newSource()">';
             break;
         case 'ip':
             //text
@@ -35,14 +35,15 @@ function selected(el) {
 
 var i = 0;
 function newSource() {
-    document.getElementById('center-part').innerHTML = '<div style="display: flex"><a>Новый источник:</a><input type="button" class="buttonStyle" onclick="newCity()" value="Добавить город"><br></div>';
+    document.getElementById('center-top').innerHTML = '<div><p style="text-align: center">Новый источник:</p><br></div>';
     newCity();
+    document.getElementById('center-bottom').innerHTML = '<input type="button" class="buttonStyle" value="Сохранить" style="float: right"><input type="button" class="buttonStyle" onclick="newCity()" value="Добавить город" style="float: right">';
 }
 function newNameSource(el) {
     document.getElementById(el.parentElement.id).innerHTML += '<input type="text" class="inputField"><br>';
 }
 function newCity() {
-    document.getElementById('center-part').innerHTML+='<br><div style="display: flex"><div style="line-height: 1.4em; margin-left: 20px"><a>Название:  </a><br><br>' +
+    document.getElementById('center-top').innerHTML+='<br><div style="display: flex"><div style="line-height: 1.4em; margin-left: 20px"><a>Название:  </a><br><br>' +
         '<a>Город:  </a><br>' +
         '<a>site_url:  </a><br>' +
         '<a>host:  </a><br>' +
