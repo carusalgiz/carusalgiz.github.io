a37be416795f7bb97ab331d15da0244b40e86d10
Karina Milovanova <frankarenyan@gmail.com>
2018-06-17 17:12:19 +1000

    3

--- script.js
+++ script.js
@@ -14,22 +14,32 @@ function selected(el) {
             document.getElementById('center-bottom').innerHTML='';
             break;
         case 'ip':
-            document.getElementById('center-top').innerHTML='<table><tr><th>Список IP-адресов:</th></tr><tr style="border-top: #d1d3d4 1px solid"><td>[адрес]</td></tr><tr style="border-top: #d1d3d4 1px solid"><td>[адрес]</td></tr><tr style="border-top: #d1d3d4 1px solid"><td>[адрес]</td></tr></table>';
+            document.getElementById('center-top').innerHTML='<table><tr><th>Список IP-адресов:</th></tr><tr><td>[адрес]</td></tr><tr><td>[адрес]</td></tr><tr><td>[адрес]</td></tr></table>';
             document.getElementById('center-bottom').innerHTML='';
             break;
-        case 'parcer':
+        case 'parser':
             document.getElementById('center-top').innerHTML='<div style="display: flex"><div><a>Источник: </a><br><select class="select-style"><option>--Source--</option><option>avito</option><option>irr</option><option>farpost</option></select></div><div style="margin-left: 10px"><a>URL страницы: </a> <br><input type="text" class="inputField"><input type="button" value="Проверить" class="buttonStyle" style="margin-left: 30px"></div></div>' +
                 '<table><tr><th colspan="2">Полученные данные: </th></tr><tr><td style="width: 120px">Источник: </td><td>[название]</td></tr><tr><td>Город: </td><td>[город]</td></tr><tr><td>Адрес: </td><td>[улица,дом, квартира]</td></tr><tr><td>Этаж: </td><td>[номер этажа]</td></tr><tr><td>Этажность: </td><td>[всего этажей]</td></tr></table>';
             document.getElementById('center-bottom').innerHTML='';
             break;
         case 'activeTasks':
-            //text
+            document.getElementById('center-top').innerHTML='<table><tr><th colspan="6">Текущие активные задачи в очереди:</th></tr><tr><td style="width: 120px">enqueue_task</td><td>avito</td><td>2018-04-19T10:40:25</td><td>2018-04-19T10:40:42</td><td style="width: 40px">0</td><td>[{"media": "avito", "category": "/komsomolsk-na-amure/zemelnye_uchastki/prodam", "location": "kms"}]</td></tr><tr><td>enqueue_task</td><td>avito</td><td>2018-04-19T10:40:25</td><td>2018-04-19T10:40:42</td><td>0</td><td>[{"media": "avito", "category": "/nahodka/kvartity/sdam”,“location”: ”nahodka”}]</td></tr><tr><td>enqueue_task</td><td>avito</td><td>2018-04-19T10:40:25</td><td>2018-04-19T10:40:42</td><td>0</td><td>[{"media": "avito", "category": "/nahodka/komnaty/sdam</td></tr></table>';
+            document.getElementById('center-bottom').innerHTML='';
+            break;
+        case 'activeSources':
+            document.getElementById('center-top').innerHTML='<table><tr><th>Активные источники:</th></tr><tr><td style="width: 140px">[название]</td><td>[перечень городов]</td></tr><tr><td>[название]</td><td>[перечень городов]</td></tr><tr><td>[название]</td><td>[перечень городов]</td></tr><tr><td>[название]</td><td>[перечень городов]</td></tr></table>';
+            document.getElementById('center-bottom').innerHTML='';
             break;
         case 'loadOnNetwork':
             //text
             break;
         case 'stats':
-            //text
+            document.getElementById('center-top').innerHTML= '<div style="display: flex"><div style="flex: 0 0 80%"><select class="select-style"><option>--[ID/URL]--</option><option>ID</option><option>URL</option></select><input type="text" class="inputField"><input type="button" value="Показать" class="buttonStyle" style="margin-left: 30px"></div><div style="flex: 0 0 20%""><a>Всего объявлений: </a><a style="margin-right: 20px">9999</a><br><a>Успешно разобранных: </a><a style="margin-right: 20px">9999</a><br></div></div>' +
+                '<div><a>От: </a><input type="date" placeholder="Дата" class="select-style"  style="margin-right: 20px"><a>До: </a><input type="date" placeholder="Дата" class="select-style"  style="margin-right: 20px"><select class="select-style"><option>--[Город]--</option><option>Хабаровск</option><option>Находка</option><option>Владивосток</option><option>Москва</option></select></div>' +
+                '<table><tr><th>ID</th><th>Тип</th><th>Город</th><th>Улица</th><th>Кол-во комнат</th><th>Этаж</th><th>Этажность</th><th>Ссылка</th></tr>' +
+                '<tr><td>1</td><td>Комната</td><td>Москва</td><td>[название улицы]</td><td>1</td><td>2</td><td>5</td><td>/moskva/komnaty/komn...........................</td></tr>' +
+                '<tr><td>2</td><td>Комната</td><td>Москва</td><td>[название улицы]</td><td>3</td><td>13</td><td>16</td><td>/moskva/kvartiry/1-k_kvartira_40_m_35</td></tr>' +
+                '<tr><td>3</td><td>Комната</td><td>Находка</td><td>[название улицы]</td><td>1</td><td>4</td><td>9</td><td>/nahodka/kvartiry/2-k_kvartira_50_m_40</td></tr></table>';
             break;
         case 'errors':
             //text
